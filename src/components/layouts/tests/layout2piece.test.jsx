48ecd722ab9ae3f911a0acf1a6522c0876606f00
Jan-Christiaan Basson <jan.basson@gmail.com>
2020-01-26 15:37:35 +1100

    add one missing test for Layout2Piece component

--- src/components/layouts/tests/layout2piece.test.jsx
+++ src/components/layouts/tests/layout2piece.test.jsx
@@ -4,7 +4,7 @@ import Layout2Piece from "../layout2piece";
 import MockTheme from "../../themes/mockTheme";
 
 describe("components/layouts/layout2piece", () => {
-  it("should display a header", () => {
+  it("should display a header placeholder", () => {
     const { container } = render(
       <MockTheme>
         <Layout2Piece />
@@ -12,5 +12,15 @@ describe("components/layouts/layout2piece", () => {
     );
     const header = container.querySelector("header");
     expect(header).toBeTruthy();
+  });
+
+  it("should display a main content placeholder", () => {
+    const { container } = render(
+      <MockTheme>
+        <Layout2Piece />
+      </MockTheme>
+    );
+    const main = container.querySelector("main");
+    expect(main).toBeTruthy();
   });
 });
