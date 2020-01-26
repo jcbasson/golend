d95b9bbac07fe1b284714cd9d17992155d67d03e
Jan-Christiaan Basson <jan.basson@gmail.com>
2020-01-26 19:30:40 +1100

    adjust golend logo dimensions

--- src/components/logos/golend/index.jsx
+++ src/components/logos/golend/index.jsx
@@ -1,17 +1,32 @@
 import React from "react";
+import styled from "styled-components";
 import { getSrcSet } from "./utils";
+
 const GolendLogo = () => {
-  const GolendLogoSrcSmall = getSrcSet({ width: 200, additional: ["f_auto"] });
-  const GolendLogoSrcLarge = getSrcSet({ width: 256, additional: ["f_auto"] });
+  const GolendLogoSrcSmall = getSrcSet({
+    width: 80,
+    additional: ["f_auto", "c_scale"]
+  });
+  const GolendLogoSrcLarge = getSrcSet({
+    width: 160,
+    additional: ["f_auto", "c_scale"]
+  });
 
   return (
-    <img
+    <LogoImg
       srcSet={`${GolendLogoSrcSmall},
             ${GolendLogoSrcLarge}`}
-      sizes="(max-width: 425px) 200px, 256px"
+      sizes="(max-width: 425px) 90px, 180px"
       alt="Golend"
     />
   );
 };
+
+const LogoImg = styled.img`
+  height: 38px;
+  @media (max-width: 425px) {
+    height: 19px;
+  }
+`;
 
 export default GolendLogo;
--- src/components/navigation/golendNavigation.js
+++ src/components/navigation/golendNavigation.js
@@ -15,6 +15,7 @@ const NavigationContainer = styled.div`
   height: 100%;
   display: flex;
   justify-content: space-between;
+  align-items: center;
 `;
 
 export default GolendNavigation;
